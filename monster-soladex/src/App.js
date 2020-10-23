c5674851c711fb20dd73893d0246328570322d58
ronniekiyegga <ronniekiyegga@hotmail.com>
2020-10-23 11:06:01 +0100

    added a onChange event to filter monster cards

--- monster-soladex/src/App.js
+++ monster-soladex/src/App.js
@@ -2,25 +2,40 @@ import React, { Component } from 'react';
 import './App.css';
 import { Cardlist } from './components/card-list/card-list.component';
 
+// https://jsonplaceholder.typicode.com/users
+
 class App extends Component {
   constructor() {
     super();
 
     this.state = {
-      monsters: []
+      monsters: [],
+      searchField: ''
     };
   }
 
+  //use fetch and promises to make an API call
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }));
   }
 
-  render() {
+  render(){
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster => 
+      monster.name.toLowerCase().includes(searchField)
+      )
+
     return (
       <div className="App">
-        <Cardlist monsters={this.state.monsters}/>
+        <input
+          type="search"
+          placeholder="search monsters"
+          onChange={e => this.setState({ searchField: e.target.value.toLocaleLowerCase()})
+          }
+        />
+        <Cardlist monsters={filteredMonsters} />
       </div>
     );
   }
